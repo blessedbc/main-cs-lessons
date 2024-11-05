ca104d709b83bc57256f40dbd2bbc905b520666d
CHRIS <NWAKAUC1@GMAIL.COM>
2024-11-05 20:23:55 +0200

    cbnls

--- array variables,foreach and if statements/Program.cs
+++ array variables,foreach and if statements/Program.cs
@@ -10,6 +10,7 @@ namespace array_variables_foreach_and_if_statements
     {
         static void Main(string[] args)
         {
+            /*
             // initialize variables - graded assignments
             int currentAssignments = 5;
 
@@ -64,6 +65,32 @@ namespace array_variables_foreach_and_if_statements
             Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
             Console.WriteLine("press the Enter key to continue");
+            */
+
+            // initialize variables - graded assignments
+            int currentAssignments = 5;
+
+            int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
+            int[] andrewScores = new int[] {92, 89, 81, 96, 90};
+            int[] emmaScores = new int[] {90, 85, 87, 98, 68};
+            int[] loganScores = new int[] {90, 95, 87, 88, 96};
+
+            int sophiaSum = 0;
+
+            decimal sophiaScore;
+
+            foreach (int Score in sophiaScores)
+            {
+                // add the exam score to the sum
+                sophiaSum += Score;
+            }
+
+            sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+            Console.WriteLine("Student\t\tGrade\n");
+            Console.WriteLine("Student\t\t" + sophiaScore + "\tA-");
+
+            Console.WriteLine("Press the Enter Key to continue");
 
             Console.ReadLine();
         }
